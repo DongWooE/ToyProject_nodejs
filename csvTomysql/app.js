8bf9bc2148a13844b3155bab66f7badcca70e304
DongWooE <ehddn977@naver.com>
2021-04-03 16:33:05 +0900

    Add set-up files

---
+++ csvTomysql/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url} router not existed`);
+    error.status = 404;
+    next(error);
+  });
+  
+//error handler
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: err
+    });
+});
+
+//Start server
+app.listen(app.get('port'), () => {
+    console.log(
+        `Listening http://localhost:${app.get('port')} in ${app.get('env')} mode!!!`);
+});
